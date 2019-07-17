a12c63f53453dbb331380f759102f9dc44a454a0
Thadee Trompetter <thadee@voorhoede.nl>
2019-07-17 15:35:12 +0200

    remove file

---
+++ scripts/data/csv/index.js
@@ -0,0 +1,24 @@
+const { readFileSync, writeFile } = require('fs')
+const csvParser = require('csv-parse/lib/sync')
+const transformSpaces = require('./transform-spaces')
+const transformBuildings = require('./transform-buildings')
+const parserOptions = require('./config')
+const fileContents = readFileSync('./data/studieplekken-mini.csv', 'utf8')
+const parsed = csvParser(fileContents, parserOptions)
+
+const spaces = transformSpaces(parsed)
+const buildings = transformBuildings(parsed)
+
+const writeSpaces = (lang, contents) => {
+  const stringifiedData = JSON.stringify(contents, null, 2)
+  return new Promise((resolve, reject) => {
+    writeFile(`./src/static/data/${lang}/spaces.json`, stringifiedData, 'utf8', (err) => {
+      if (err) {
+        return reject(err)
+      }
+      resolve()
+    })
+  })
+}
+
+Promise.all([ writeSpaces('nl', spaces), writeSpaces('en', spaces) ])
