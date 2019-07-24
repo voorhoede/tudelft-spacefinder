9dbca5be5ef5c5d37292d063a9d77767cf401380
Thadee Trompetter <thadee@voorhoede.nl>
2019-07-24 13:19:39 +0200

    write data files

--- scripts/data/index.js
+++ scripts/data/index.js
@@ -1,3 +1,4 @@
+const { writeFile } = require('fs')
 const getDataFromCms = require('./cms')
 const { getData: getDataFromCsv, transform } = require('./csv')({
   csvPath: './data/studieplekken-mini.csv'
@@ -8,16 +9,23 @@ Promise.all([ getDataFromCsv(), getDataFromCms() ])
   // @TODO: write data some place sensible
   .then(r => console.log(r.buildings))
 
-// const writeSpaces = (lang, contents) => {
-//   const stringifiedData = JSON.stringify(contents, null, 2)
-//   return new Promise((resolve, reject) => {
-//     writeFile(`./src/static/data/${lang}/spaces.json`, stringifiedData, 'utf8', (err) => {
-//       if (err) {
-//         return reject(err)
-//       }
-//       resolve()
-//     })
-//   })
-// }
+const writeFiles = (files = []) => {
+  return Promise.all(files.map(({ name, contents }) => {
+    const stringifiedData = JSON.stringify(contents)
+    return new Promise((resolve, reject) => {
+      writeFile(`./src/static/data/${name}.json`, stringifiedData, 'utf8', (err) => {
+        if (err) {
+          return reject(err)
+        }
+        resolve()
+      })
+    })
+  }))
+}
 
-// Promise.all([ writeSpaces('nl', spaces), writeSpaces('en', spaces) ])
+Promise.all([ getDataFromCsv(), getDataFromCms() ])
+  .then(transform)
+  .then(({ spaces, buildings }) => writeFiles([
+    { name: 'spaces', contents: spaces },
+    { name: 'buildings', contents: buildings }
+  ]))
