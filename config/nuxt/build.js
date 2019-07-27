d1ea87fdd633ab78dbb1fc6c863e4b76e7c00e3c
Jasper Moelker <j.b.moelker@activeids.nl>
2019-07-27 18:33:37 +0200

    Remove unused vue-svg-loader

--- config/nuxt/build.js
+++ config/nuxt/build.js
@@ -2,25 +2,12 @@
  * @see https://nuxtjs.org/api/configuration-build
  */
 module.exports = {
-    postcss: {
-      'postcss-import': {},
-      'postcss-custom-properties': {
-        importFrom: './src/components/app-core/variables.css',
-        preserve: false,
-      },
-      'autoprefixer': {}
+  postcss: {
+    'postcss-import': {},
+    'postcss-custom-properties': {
+      importFrom: './src/components/app-core/variables.css',
+      preserve: false
     },
-    extend (config) {
-      config.module.rules.forEach((rule) => {
-        if (rule.test.toString() === '/\\.(png|jpe?g|gif|svg|webp)$/i') {
-          rule.test = /\\.(png|jpe?g|gif|webp)$/i
-        }
-      })
-
-      config.module.rules.push({
-        test: /\.svg$/,
-        loader: 'vue-svg-loader',
-      })
-    },
+    'autoprefixer': {}
   }
-  
+}
