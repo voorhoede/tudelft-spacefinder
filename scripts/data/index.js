d29e639635776580bfdc29cfc05d0fb2afb1c272
Thadee Trompetter <thadee@voorhoede.nl>
2019-07-31 15:59:05 +0200

    move schema validation out of transformation functions

--- scripts/data/index.js
+++ scripts/data/index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const { writeFile } = require('fs')
 const getDataFromCms = require('./cms')
 const { addOpeningHours } = require('./exchange')
+const validate = require('./validate')
 
 const { CSV_PATH: csvPath } = process.env
 
@@ -25,6 +26,7 @@ const writeFiles = (files = []) => {
 Promise.all([ getDataFromCsv(), getDataFromCms() ])
   .then(transform)
   .then(addOpeningHours)
+  .then(validate)
   .then(({ spaces, buildings }) => writeFiles([
     { name: 'spaces', contents: spaces },
     { name: 'buildings', contents: buildings }
