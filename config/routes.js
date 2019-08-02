4741035bc3bd4a7f639aa8249c01337ea3914641
Jasper Moelker <j.b.moelker@activeids.nl>
2019-08-02 17:21:50 +0200

    Generate static version of all space pages
    and correctly redirect non-existing routes both client- and server-side.

--- config/routes.js
+++ config/routes.js
@@ -3,6 +3,7 @@ import flattenDeep from 'lodash/flattenDeep'
 import pagesI18n from '../src/pages.i18n'
 const locales = ['en', 'nl']
 const buildings = require('../src/static/data/buildings.json')
+const spaces = require('../src/static/data/spaces.json')
 
 const rootRoutes = locales.map(locale => {
   const route = pagesI18n['buildings/index'][locale]
@@ -17,9 +18,20 @@ const buildingRoutes = flattenDeep(buildings.map(building => {
   })
 }))
 
+const spaceRoutes = flattenDeep(spaces.map(space => {
+  return locales.map(locale => {
+    const building = buildings.find((building) => building.number === space.buildingNumber)
+    const route = pagesI18n['buildings/_buildingSlug/spaces/_spaceSlug'][locale]
+      .replace(':buildingSlug', building.i18n[locale].slug)
+      .replace(':spaceSlug', space.slug)
+    return `/${locale}${route}`
+  })
+}))
+
 const routes = [
   ...rootRoutes,
   ...buildingRoutes,
+  ...spaceRoutes
 ]
 
 export default routes
