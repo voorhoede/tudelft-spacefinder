d73c187b7ef22c6afc9330d5f5e2192e25e5116d
Thadee Trompetter <thadee@voorhoede.nl>
2019-08-07 15:38:54 +0200

    add location-is-open filter

--- src/lib/filter-spaces.js
+++ src/lib/filter-spaces.js
@@ -6,7 +6,7 @@ export default function ({ filters, spaces }) {
 }
 
 function getActiveFilterKeys(filters, keys) {
-  return keys.filter(key => {
+  return keys.filter((key) => {
     const data = filters[key]
     return Array.isArray(data) ? !!data.length : data
   })
@@ -16,12 +16,32 @@ function filterSpaces(filters, spaces, activeFilterKeys) {
   return spaces.filter(space => filterSpace(filters, space, activeFilterKeys))
 }
 
+/*
+  If the current time falls in any of the time ranges for today, we're in
+  business
+*/
+function spaceIsOpen(now, openingHours) {
+  // first item in openingHours array is today
+  const [ { time: timeRanges } ] = openingHours
+  return timeRanges
+    // parse to date objects for comparison
+    .map(range => range.map(item => new Date(item)))
+    .some(([ start, end ]) => {
+      return now >= start && now <= end
+    })
+}
+
 function filterSpace(filters, space, activeFilterKeys) {
-  return activeFilterKeys.every(activeFilterKey => {
+  const now = new Date()
+  return activeFilterKeys.every((activeFilterKey) => {
     const facility = space.facilities[activeFilterKey]
 
     if (Array.isArray(filters[activeFilterKey])) {
       return filters[activeFilterKey].includes(facility)
+    }
+
+    if (activeFilterKey === 'showOpenLocations') {
+      return spaceIsOpen(now, space.openingHours)
     }
 
     return facility
