6e4a7d88e7b39a3deb10f850b8474ca631a96264
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2019-08-08 18:15:19 +0200

    Show notification bar when the website is openend in IE11 or lower (#79)
    * Add notification bar when the website is openend in IE11 or lower
    
    * Run a hardcoded script in the head to display message correctly in ie11 and below
    
    * Refactor ie check and "no support" notification

---
+++ src/components/notification-bar/notification-bar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="notification-bar">
+    {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    message: String,
+  },
+}
+</script>
+
+<style>
+@import '../app-core/variables.css';
+
+.notification-bar {
+  z-index: var(--layer--popup);
+  position: absolute;
+  padding: var(--spacing-default);
+  bottom: var(--spacing-default);
+  left: var(--spacing-default);
+  width: calc(100% - var(--spacing-double));
+  background: var(--brand-secondary-color);
+  border: 1px solid transparent;
+  box-shadow: var(--shadow-small);
+  text-align: center;
+  color: var(--background-color);
+}
+
+@media (min-width: 700px) {
+  .notification-bar {
+    bottom: var(--spacing-double);
+    left: var(--spacing-double);
+    width: calc(100% - 2 * var(--spacing-double));
+  }
+}
+</style>
