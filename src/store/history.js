dc121d08bcd35275b98bc94bbc43abaa70a63053
Thadee Trompetter <thadee@voorhoede.nl>
2019-08-13 11:21:36 +0200

    only stack route paths to keep history

--- src/store/history.js
+++ src/store/history.js
@@ -1,26 +1,25 @@
-const copyObject = (object) => { return JSON.parse(JSON.stringify(object)) }
-
 export const state = () => ({
-  routes: []
+  routePaths: []
 })
 
 export const mutations = {
   addRoute(state, route) {
-    state.routes = [...state.routes, copyObject(route)]
+    const { fullPath } = route
+    state.routePaths = [...state.routePaths, fullPath]
   },
   goBack(state) {
-    state.routes = state.routes.slice(0, -1)
+    state.routePaths = state.routePaths.slice(0, -1)
     window.history.back()
   }
 }
 
 export const getters = {
   hasHistory: (state) => {
-    return state.routes.length > 1
+    return state.routePaths.length > 1
   },
   previousPageUrl: (state, getters) => {
     if (!getters.hasHistory) return
-    const previousRoute = state.routes[state.routes.length - 2]
-    return previousRoute.fullPath
+    const previousRoutePath = state.routePaths[state.routePaths.length - 2]
+    return previousRoutePath
   }
 }
