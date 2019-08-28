f6f92b3eb1d02463d2195bbc040810fd2d6fa43a
Bas de Vleeschhouwer <hallo@basdevleeschhouwer.nl>
2019-08-28 14:45:11 +0200

    Tooltips (#111)
    * Set up plugin for tooltips
    
    * Style tooltip & add correct copy
    
    * Set z-index using variable
    
    * Run tooltip client side only

--- src/components/space-facilities/space-facilities.vue
+++ src/components/space-facilities/space-facilities.vue
@@ -8,6 +8,10 @@
       <svg-icon
         :name="getIconName(facility)"
         class="space-facility__icon"
+        v-tooltip="{
+          content: $t(getFacilityValue(facility)),
+          trigger: 'hover click focus'
+        }"
       />
 
       <span class="a11y-sr-only">
@@ -48,12 +52,52 @@ export default {
 </script>
 
 <style>
+@import '../app-core/variables.css';
+
 .space-facility__item {
+  position: relative;
   display: inline-block;
 }
 
 .space-facility__icon {
   width: 25px;
   height: 25px;
+}
+
+.tooltip {
+  display: block;
+  z-index: var(--layer--popup);
+}
+
+.tooltip-inner {
+  padding: var(--spacing-quarter) var(--spacing-half);
+  background: var(--brand-secondary-color);
+  font-size: var(--font-size-smaller);
+  color: var(--background-color);
+}
+
+.tooltip-arrow {
+  position: absolute;
+  z-index: 1;
+  margin: 5px;
+  width: 0;
+  height: 0;
+  border-style: solid;
+  border-color: var(--brand-secondary-color);
+}
+
+.tooltip[x-placement^="top"] {
+  margin-bottom: 5px;
+}
+
+.tooltip[x-placement^="top"] .tooltip-arrow {
+  margin-top: 0;
+  margin-bottom: 0;
+  bottom: -5px;
+  left: calc(50% - 5px);
+  border-width: 5px 5px 0 5px;
+  border-left-color: transparent;
+  border-right-color: transparent;
+  border-bottom-color: transparent;
 }
 </style>
