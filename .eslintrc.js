8bd4436b327f15ca75fe2a493b77349c5b500e8e
Jasper Moelker <j.b.moelker@activeids.nl>
2019-09-17 20:35:19 +0200

    Use sonar-lint to detect code smells

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,11 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint',
   },
+  plugins: ['sonarjs'],
   extends: [
     '@nuxtjs',
     'plugin:nuxt/recommended',
+    'plugin:sonarjs/recommended',
   ],
   rules: {
     'no-console': ['warn', { allow: ['error', 'info'] }],
