3e8322c63102957c1a5cefd343267698c1b0b2e4
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2019-12-19 10:17:55 +0100

    Implement new design for number of seats in space cards (#128)
    * Implement new seating design in space card
    
    * Implement new seating design in space detail card
    
    * Make copy short in spaces overview to prevent crowding in cards

--- src/components/space-card/space-card.vue
+++ src/components/space-card/space-card.vue
@@ -8,11 +8,12 @@
     "
     class="space-card card"
   >
+    <h3 v-if="space.name">
+      {{ space.name }}
+    </h3>
+
     <div class="space-card__info">
       <div class="space-card__location">
-        <h3 v-if="space.name">
-          {{ space.name }}
-        </h3>
         <p
           v-if="space.name"
           class="space-card__description"
@@ -24,31 +25,29 @@
         </h3>
       </div>
 
-      <h4 class="a11y-sr-only">
-        {{ $t('seating') }}
-      </h4>
-
-      <div class="flat-list space-card__seating">
-        <svg-icon name="seat-icon" class="space-card__seating-icon" />
-        {{ space.seats }}
-      </div>
-    </div>
-
-    <div class="space-card__meta">
       <card-status
         :opening-hours="space.openingHours"
         :is-open="locationIsOpen"
         class="space-card__status"
       />
+    </div>
 
-      <h4 class="a11y-sr-only">
-        {{ $t('facilities') }}
-      </h4>
+    <h4 class="a11y-sr-only">
+      {{ $t('facilities') }}
+    </h4>
 
-      <space-facilities
-        :facilities="space.facilities"
-        class="space-card__facilities"
-      />
+    <space-facilities
+      :facilities="space.facilities"
+      class="space-card__facilities"
+    />
+
+    <h4 class="a11y-sr-only">
+      {{ $t('seating') }}
+    </h4>
+
+    <div class="space-card__seating">
+      <svg-icon name="seat-icon" class="space-card__seating-icon" />
+      {{ space.seats }} {{ $t('seatsDescriptionShort') }}
     </div>
   </nuxt-link>
 </template>
@@ -105,38 +104,22 @@ export default {
   font-style: normal;
 }
 
-.space-card__seating {
+.space-card__status {
   flex: 0 0 auto;
-  margin-left: var(--spacing-default);
-  font-size: var(--font-size-smaller);
-  font-weight: bold;
 }
 
-.space-card__seating li {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+.space-card__facilities {
+  margin-left: -.2rem;
+}
+
+.space-card__seating {
+  font-size: var(--font-size-smaller);
 }
 
 .space-card__seating-icon {
   margin: -2px 1px 0 0;
-  width: 18px;
-  height: 18px;
+  width: 16px;
+  height: 16px;
   vertical-align: middle;
-}
-
-.space-card__meta {
-  display: flex;
-}
-
-.space-card__status {
-  order: 2;
-  flex: 0 0 auto;
-  margin-top: .1rem;
-}
-
-.space-card__facilities {
-  order: 1;
-  flex: 1 1 auto;
 }
 </style>
