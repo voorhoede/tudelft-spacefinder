5475d7c69929660144f1771511f83093573cb608
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-02-28 11:11:14 +0100

    feat: migrate to Nuxt 3 - PWA module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import routes from "./config/routes";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   srcDir: "src/",
-  modules: ["@pinia/nuxt", "@vueuse/nuxt"],
+  modules: ["@pinia/nuxt", "@vueuse/nuxt", "@kevinmarrec/nuxt-pwa"],
   css: ["normalize.css", "@/components/app-core/index.css"],
   build: {
     transpile: ["floating-vue"],
@@ -18,5 +18,24 @@ export default defineNuxtConfig({
     prerender: {
       routes: ["/sitemap.xml", "/robots.txt", ...routes],
     },
+  },
+  pwa: {
+    manifest: {
+      name: "TU Delft\nSpacefinder",
+      short_name: "Spacefinder",
+      background_color: "#00a6d6",
+      theme_color: "#00a6d6",
+      display: "standalone",
+      crossorigin: "use-credentials",
+    },
+    // workbox: {
+    //   runtimeCaching: [
+    //     {
+    //       urlPattern: 'https://www.datocms-assets.com/.*',
+    //       handler: 'cacheFirst',
+    //       method: 'GET',
+    //     },
+    //   ],
+    // },
   },
 });
