8158eefc709a5295d5dc561d26c0a906f58b0969
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-01 04:11:55 +0100

    fix: robots.txt content type

--- src/server/routes/robots.txt.ts
+++ src/server/routes/robots.txt.ts
@@ -2,5 +2,6 @@ export default defineEventHandler(async (event) => {
   const runtimeConfig = useRuntimeConfig();
   let baseUrl = runtimeConfig.public.baseUrl;
   if (!baseUrl.endsWith("/")) baseUrl += "/";
+  setResponseHeader(event, "Content-Type", "text/plain");
   return `Sitemap: ${baseUrl}sitemap.xml`;
 });
