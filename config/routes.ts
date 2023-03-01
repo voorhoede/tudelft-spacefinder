1467eaa7d2e6b4a44952749166d3f2315226d51f
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-01 17:20:46 +0100

    refactor: string interpolation instead of concat

--- config/routes.ts
+++ config/routes.ts
@@ -16,27 +16,19 @@ const routes = [
 ];
 
 for (const building of buildings) {
-  routes.push("/en/buildings/" + building.i18n.en.slug + "/");
-  routes.push("/en/buildings/" + building.i18n.en.slug + "/spaces/");
-  routes.push("/nl/gebouwen/" + building.i18n.nl.slug + "/");
-  routes.push("/nl/gebouwen/" + building.i18n.nl.slug + "/ruimtes/");
+  routes.push(`/en/buildings/${building.i18n.en.slug}/`);
+  routes.push(`/en/buildings/${building.i18n.en.slug}/spaces/`);
+  routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/`);
+  routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/ruimtes/`);
 }
 
 for (const space of spaces) {
   const building = buildingMap[space.buildingNumber];
   routes.push(
-    "/en/buildings/" +
-      building.i18n.en.slug +
-      "/spaces/" +
-      space.i18n.en.slug +
-      "/"
+    `/en/buildings/${building.i18n.en.slug}/spaces/${space.i18n.en.slug}/`
   );
   routes.push(
-    "/nl/gebouwen/" +
-      building.i18n.nl.slug +
-      "/ruimtes/" +
-      space.i18n.nl.slug +
-      "/"
+    `/nl/gebouwen/${building.i18n.nl.slug}/ruimtes/${space.i18n.nl.slug}/`
   );
 }
 
