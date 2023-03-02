e0e07d4e56a78ffde0d41aeabf1071ece81b471a
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-02 16:52:23 +0100

    feat: error page for dev

---
+++ src/error.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="error-layout">
+    <h1>
+      {{ error.statusCode == 404 ? "Page not found" : "An error occurred" }}
+    </h1>
+    <button @click="handleError" class="button button--primary" href="~/">
+      Return home
+    </button>
+  </section>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{ error: { statusCode: number } }>();
+const handleError = () => clearError({ redirect: "/" });
+</script>
+
+<style>
+.error-layout {
+  height: 100vh;
+  width: 100vw;
+  background-color: var(--background-color);
+  padding-top: 100px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
--- src/middleware/set-locale.global.ts
+++ src/middleware/set-locale.global.ts
@@ -7,6 +7,12 @@ export default defineNuxtRouteMiddleware((to, from) => {
     typeof to.params.locale == "string" &&
     to.params.locale != $locale.value
   ) {
+    if (!["en", "nl"].includes(to.params.locale))
+      throw createError({
+        statusCode: 404,
+        statusMessage: "Page not found",
+        fatal: true,
+      });
     $locale.value = to.params.locale;
     Cookie.set("nf_lang", to.params.locale);
   }
