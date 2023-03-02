ca73a11744ea51bfa19dee5dc7e6ecd9045f1246
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-02 06:21:55 +0100

    chores: comment

--- src/middleware/setLocale.global.ts
+++ src/middleware/setLocale.global.ts
@@ -8,7 +8,6 @@ export default defineNuxtRouteMiddleware((to, from) => {
     to.params.locale != $locale.value
   ) {
     $locale.value = to.params.locale;
-    //TODO: cookie is now set in situations where user navigates via URL (wasn't the case before). Is that OK?
     Cookie.set("nf_lang", to.params.locale);
   }
 });
