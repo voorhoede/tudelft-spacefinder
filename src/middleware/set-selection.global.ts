6045eb542609eb006925cab46b5861257c2df3c2
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-07 15:51:06 +0100

    feat: a more concise store

--- src/middleware/set-selection.global.ts
+++ src/middleware/set-selection.global.ts
@@ -6,7 +6,10 @@ export default defineNuxtRouteMiddleware((to, from) => {
   if (process.client && nuxtApp.isHydrating && nuxtApp.payload.serverRendered)
     return; // Store has been set up on server
   if (to.params.spaceSlug) {
-    store.selectSpace(to.params.spaceSlug as string);
+    store.currentSelection = {
+      level: "space",
+      spaceSlug: to.params.spaceSlug as string,
+    };
     if (!store.currentSpace) {
       throw createError({
         statusCode: 404,
@@ -15,12 +18,15 @@ export default defineNuxtRouteMiddleware((to, from) => {
       });
     }
   } else if (to.params.buildingSlug) {
-    store.selectBuilding(to.params.buildingSlug as string);
+    store.currentSelection = {
+      level: "building",
+      buildingSlug: to.params.buildingSlug as string,
+    };
     if (!store.currentBuilding)
       throw createError({
         statusCode: 404,
         statusMessage: "Page not found",
         fatal: true,
       });
-  } else store.clearSelection();
+  } else store.currentSelection = undefined;
 });
