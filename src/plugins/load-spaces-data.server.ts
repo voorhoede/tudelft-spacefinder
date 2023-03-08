14e8d69c90edc056c87833129198694186123355
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-08 16:10:05 +0100

    chore: less todos

--- src/plugins/load-spaces-data.server.ts
+++ src/plugins/load-spaces-data.server.ts
@@ -3,9 +3,9 @@ import { useSpacesStore } from "~/stores/spaces";
 export default defineNuxtPlugin((app) => {
   const spacesStore = useSpacesStore(app.$pinia);
   import("~/data/buildings.json").then((buildings) => {
-    spacesStore.setBuildings(buildings.default as any); //TODO: arrays can't become tuples, silly things
+    spacesStore.setBuildings(buildings.default as any);
   });
   import("~/data/spaces.json").then((spaces) => {
-    spacesStore.setSpaces(spaces.default as any); //TODO: arrays can't become tuples, silly things
+    spacesStore.setSpaces(spaces.default as any);
   });
 });
