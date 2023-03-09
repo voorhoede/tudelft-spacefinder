012830c892b68e65c898da501ebbc52f733cebf8
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-09 11:37:56 +0100

    feat: get/subscribe to occupancy data for spaces

--- src/composables/useSpacefinderSupabase.ts
+++ src/composables/useSpacefinderSupabase.ts
@@ -2,6 +2,7 @@ import { Database } from "../types/supabase";
 
 export default function useSpacefinderSupabase() {
   const client = useSupabaseClient<Database>();
+
   async function getBuildingsOccupancyCurrent() {
     const { data, error, status } = await client
       .from("buildings_latest_states")
@@ -13,6 +14,7 @@ export default function useSpacefinderSupabase() {
     }
     return activeDevicesPerBuilding;
   }
+
   function subscribeToBuildingsOccupancy(
     callback: (buildingNumber: number, deviceCount: number) => void
   ) {
@@ -32,5 +34,56 @@ export default function useSpacefinderSupabase() {
     buildingsRealtimeChannel.subscribe();
   }
 
-  return { getBuildingsOccupancyCurrent, subscribeToBuildingsOccupancy };
+  async function getSpacesOccupancyCurrent() {
+    const { data, error, status } = await client
+      .from("spaces_latest_states")
+      .select("*");
+    const activeDevicesPerBuildingAndSpace: Record<
+      number,
+      Record<string, number>
+    > = {};
+    if (data) {
+      for (const row of data) {
+        if (!activeDevicesPerBuildingAndSpace[row.building_number])
+          activeDevicesPerBuildingAndSpace[row.building_number] = {};
+        activeDevicesPerBuildingAndSpace[row.building_number][row.room_id] =
+          row.device_count;
+      }
+    }
+    return activeDevicesPerBuildingAndSpace;
+  }
+
+  function subscribeToSpacesOccupancy(
+    callback: (
+      buildingNumber: number,
+      roomId: string,
+      deviceCount: number
+    ) => void
+  ) {
+    const spacesRealtimeChannel = client
+      .channel("spaces")
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "spaces_latest_states" },
+        ({ eventType, new: newData, old }) => {
+          if (eventType == "UPDATE" || eventType == "INSERT") {
+            callback(
+              newData.building_number,
+              newData.room_id,
+              newData.device_count
+            );
+          } else {
+            callback(old.building_number, old.room_id, 0);
+          }
+        }
+      );
+    spacesRealtimeChannel.subscribe();
+  }
+
+  return {
+    getBuildingsOccupancyCurrent,
+    subscribeToBuildingsOccupancy,
+    getSpacesOccupancyCurrent,
+    subscribeToSpacesOccupancy,
+  };
 }
--- src/plugins/load-occupancy-data.client.ts
+++ src/plugins/load-occupancy-data.client.ts
@@ -2,10 +2,18 @@ import { useSpacesStore } from "~/stores/spaces";
 export default defineNuxtPlugin(async (app) => {
   const spacesStore = useSpacesStore(app.$pinia);
   const supabase = useSpacefinderSupabase();
+
   const activeDevicesPerBuilding =
     await supabase.getBuildingsOccupancyCurrent();
   spacesStore.bulkSetBuildingOccupancy(activeDevicesPerBuilding);
   supabase.subscribeToBuildingsOccupancy((buildingNumber, deviceCount) =>
     spacesStore.setBuildingOccupancy(buildingNumber, deviceCount)
+  );
+
+  const activeDevicesPerSpace = await supabase.getSpacesOccupancyCurrent();
+  console.log(activeDevicesPerSpace);
+  spacesStore.bulkSetSpaceOccupancy(activeDevicesPerSpace);
+  supabase.subscribeToSpacesOccupancy((buildingNumber, roomId, deviceCount) =>
+    spacesStore.setSpaceOccupancy(buildingNumber, roomId, deviceCount)
   );
 });
