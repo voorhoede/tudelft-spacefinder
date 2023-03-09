8171caa4d9d0ed3123769deb3614ef95c86c7f9e
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-09 14:28:33 +0100

    chore: comment

--- src/plugins/view-mode.ts
+++ src/plugins/view-mode.ts
@@ -1,5 +1,7 @@
 export default defineNuxtPlugin((nuxtApp) => {
   const isMobile = ref(false);
+  // Lifecycle-dependend composables like useMediaQuery cannot be used directly during plugin setup
+  // so we wait for the Vue app to be mounted
   nuxtApp.hook("app:mounted", () => {
     watchEffect(() => {
       isMobile.value = !useMediaQuery("(min-width: 700px)").value;
