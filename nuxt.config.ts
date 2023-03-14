91d7aafbe056f2f735f46294f1677df6da59428e
Selwyn <talk@selwyn.cc>
2023-03-14 13:11:10 +0100

    Add secret query parameter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,6 +30,7 @@ export default defineNuxtConfig({
     transpile: ["floating-vue"],
   },
   runtimeConfig: {
+    internalSecret: process.env.INTERNAL_SECRET,
     supabaseCredentials: {
       url: process.env.SUPABASE_URL,
       key: process.env.SUPABASE_KEY,
