792bbffd282546ded40735587b961cb34339aa76
Selwyn <talk@selwyn.cc>
2023-03-15 16:53:18 +0100

    Split at maplocation as fallback

--- src/server/api/consume-wifi-data.ts
+++ src/server/api/consume-wifi-data.ts
@@ -80,7 +80,9 @@ async function consumeLastBatch({ client }: { client: SupabaseClient }) {
                 "updated_at": new Date(Number(message.timestamp)).toISOString(),
                 "access_point_name": decodedValue.name,
                 "device_count": decodedValue.clientCount,
-                "building_number": decodedValue.name.split("-").at(1) || 9001,
+                "building_number": decodedValue.name.split("-").at(1)
+                  || decodedValue.mapLocation.split(".").at(0)
+                  || 0,
                 "floor": decodedValue.locationHierarchy.split(">").at(-1).trim(),
                 "map_location": decodedValue.mapLocation,
               }))
