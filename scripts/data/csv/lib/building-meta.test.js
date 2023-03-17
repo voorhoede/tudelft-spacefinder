0494bf095be49bc0f66ef96ee11838a3cf16ff42
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-17 11:06:43 +0100

    fix: seat count, name trims, slug generation

--- scripts/data/csv/lib/building-meta.test.js
+++ scripts/data/csv/lib/building-meta.test.js
@@ -1,33 +1,8 @@
-import { fromI18n, buildingNumberFromId } from './building-meta'
+import { buildingNumberFromId } from "./building-meta";
 
-const data = {
-  someProperty: 'some value',
-  name: '13 - Architecture',
-  abbreviation: 'Arch',
-}
-
-test('fromI18n returns expected properties', () => {
-  expect(fromI18n(data)).toEqual({
-    abbreviation: 'Arch',
-    name: 'Architecture',
-    slug: '13-arch',
-  })
-})
-
-test('is able to handle bad data', () => {
-  let result = fromI18n({ foo: 'bar' })
-  expect(result).toEqual({})
-
-  result = fromI18n({ name: 'foo' })
-  expect(result).toEqual({})
-
-  result = fromI18n({ name: '12-bad', abbreviation: '' })
-  expect(result).toEqual({})
-})
-
-test('parse the building number from a building id', () => {
-  let result = buildingNumberFromId('04 - some building name')
-  expect(result).toBe(4)
-  result = buildingNumberFromId('no building number here')
-  expect(result).toBe(null)
-})
+test("parse the building number from a building id", () => {
+  let result = buildingNumberFromId("04 - some building name");
+  expect(result).toBe(4);
+  result = buildingNumberFromId("no building number here");
+  expect(result).toBe(null);
+});
