67d0cf661cc0b1e2f9b45b8602636704ed5de03e
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-17 12:23:22 +0100

    refactor: a little less ramda

--- scripts/data/exchange/rooms-to-times.js
+++ scripts/data/exchange/rooms-to-times.js
@@ -1,45 +1,15 @@
-const {
-  filter,
-  fromPairs,
-  isNil,
-  last,
-  map,
-  not,
-  pathOr,
-  pipe,
-  transpose,
-  unapply,
-} = require('ramda')
-
-const getCalendarList = pathOr([], [
-  'FreeBusyResponseArray',
-  'FreeBusyResponse',
-])
-
-const getCalendarEvents = map(pathOr(null, [
-  'FreeBusyView',
-  'CalendarEventArray',
-  'CalendarEvent',
-]))
-
-const getEvents = pipe(
-  getCalendarList,
-  getCalendarEvents
-)
-
-const calendarNotNil = pipe(
-  last,
-  isNil,
-  not
-)
-
-const getCalendarsToIdMap = pipe(
-  unapply(transpose),
-  filter(calendarNotNil),
-  fromPairs
-)
-
 module.exports = (exchangeIds, availabilityResponse) => {
-  const events = getEvents(availabilityResponse)
-  return getCalendarsToIdMap(exchangeIds, events)
-}
+  const calendarList =
+    availabilityResponse.FreeBusyResponseArray?.FreeBusyResponse ?? [];
+  const events = calendarList.map(
+    (calendar) =>
+      calendar.FreeBusyView?.CalendarEventArray?.CalendarEvent ?? null
+  );
+  const result = {};
+  for (const i = 0; i < exchangeIds.length; i++) {
+    const event = events[i];
+    if (!event) continue;
+    result[exchangeIds[i]] = event;
+  }
+  return result;
+};
--- scripts/data/exchange/soap-client.js
+++ scripts/data/exchange/soap-client.js
@@ -1,35 +1,41 @@
-const { pathOr } = require('ramda')
-const httpntlm = require('httpntlm')
-const parser = require('fast-xml-parser')
+const httpntlm = require("httpntlm");
+const parser = require("fast-xml-parser");
 
-module.exports = config => (xml) => {
+module.exports = (config) => (xml) => {
   return new Promise((resolve, reject) => {
-    httpntlm.post({
-      ...config,
-      headers: {
-        'Content-type': 'text/xml',
+    httpntlm.post(
+      {
+        ...config,
+        headers: {
+          "Content-type": "text/xml",
+        },
+        body: xml,
       },
-      body: xml,
-    }, (err, res) => {
-      if (err) {
-        return reject(err)
-      } else if (res && res.statusCode !== 200) {
-        return reject(new Error(res.body || 'failed to make SOAP to EWS call'))
-      }
+      (err, res) => {
+        if (err) {
+          return reject(err);
+        } else if (res && res.statusCode !== 200) {
+          return reject(
+            new Error(res.body || "failed to make SOAP to EWS call")
+          );
+        }
 
-      const parsed = parser.parse(res.body)
-      // Cautiously get user availability response
-      const availability = pathOr(null, [
-        's:Envelope',
-        's:Body',
-        'GetUserAvailabilityResponse',
-      ], parsed)
+        const parsed = parser.parse(res.body);
+        // Cautiously get user availability response
+        const availability =
+          parsed?.["s:Envelope"]?.["s:Body"]?.GetUserAvailabilityResponse ??
+          null;
 
-      if (availability) {
-        resolve(availability)
-      } else {
-        reject(new Error('could not find UserAvailabilityResponse in SOAP envelope'))
+        if (availability) {
+          resolve(availability);
+        } else {
+          reject(
+            new Error(
+              "could not find UserAvailabilityResponse in SOAP envelope"
+            )
+          );
+        }
       }
-    })
-  })
-}
+    );
+  });
+};
