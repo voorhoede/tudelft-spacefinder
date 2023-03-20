b1627f7e2c5eb100b09c921275a0e8807052450c
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-20 13:15:13 +0100

    refactor: TypeScript for scripts. No ramda

--- scripts/data/csv/index.ts
+++ scripts/data/csv/index.ts
@@ -1,8 +1,19 @@
 import { readFile } from "fs";
 import { parse } from "csv-parse/sync";
-import { getSpaces } from "./transform-spaces";
-import { getBuildings } from "./transform-buildings";
 import parserOptions from "./config";
+import { getBuilding } from "./transform-buildings";
+import { getSpace } from "./transform-spaces";
+import { CsvSpaceData } from "./../../../src/types/Space";
+import {
+  CsvBuildingData,
+  CsvAndCmsBuildingData,
+  CmsBuildingData,
+} from "./../../../src/types/Building";
+
+export function buildingNumberFromId(buildingId: string) {
+  const result = parseInt(buildingId);
+  return isNaN(result) ? undefined : result;
+}
 
 export function getData(csvPath: string) {
   return new Promise<Record<string, any>[]>((resolve, reject) => {
@@ -17,10 +28,35 @@ export function getData(csvPath: string) {
 
 export function transform(
   dataFromCsv: Record<string, any>[],
-  dataFromCms: Record<string, any>[]
+  dataFromCms: CmsBuildingData[]
 ) {
+  const spaces: CsvSpaceData[] = [];
+  const buildings = {} as Record<number, CsvBuildingData>;
+
+  for (const source of dataFromCsv) {
+    const buildingNumber = buildingNumberFromId(source.buildingId);
+    if (buildingNumber == null) {
+      console.error("Invalid buildingId: ", source.buildingId);
+      continue;
+    }
+    const space = getSpace(buildingNumber, source);
+    spaces.push(space);
+
+    if (!(buildingNumber in buildings))
+      buildings[buildingNumber] = getBuilding(buildingNumber, source);
+    buildings[buildingNumber].totalSeats += source.seats;
+    buildings[buildingNumber].totalSpaces += 1;
+  }
+
+  for (const cmsBuilding of dataFromCms) {
+    const building = buildings[cmsBuilding.number] as CsvAndCmsBuildingData;
+    if (!building) continue;
+    building.bounds = cmsBuilding.bounds;
+    building.image = cmsBuilding.image;
+  }
+
   return {
-    spaces: getSpaces(dataFromCsv),
-    buildings: getBuildings(dataFromCsv, dataFromCms),
+    spaces,
+    buildings: Object.values(buildings) as CsvAndCmsBuildingData[],
   };
 }
