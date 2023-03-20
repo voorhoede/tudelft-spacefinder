7639761f29002aaf079fa7576bcd2db8c6075d76
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-20 13:15:13 +0100

    refactor: TypeScript for scripts. No ramda

--- scripts/data/csv/lib/building-meta.test.ts
+++
@@ -1,8 +0,0 @@
-import { buildingNumberFromId } from "./index";
-
-test("parse the building number from a building id", () => {
-  let result = buildingNumberFromId("04 - some building name");
-  expect(result).toBe(4);
-  result = buildingNumberFromId("no building number here");
-  expect(result).toBe(undefined);
-});
