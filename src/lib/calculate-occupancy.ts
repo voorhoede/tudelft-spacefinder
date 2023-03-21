f7ade30a2f2342d3439f4736c7170d8bbac8edc7
Selwyn <talk@selwyn.cc>
2023-03-21 16:23:58 +0100

    Use occupancy limit from CMS

--- src/lib/calculate-occupancy.ts
+++ src/lib/calculate-occupancy.ts
@@ -2,10 +2,11 @@ import type { Occupancy } from "../types/Filters";
 
 export default function calculateOccupancy(
   deviceCount: number,
-  seatCount: number
+  seatCount: number,
+  occupancyLimit: number,
 ): Occupancy {
   const visitorCountEstimate = deviceCount / 2.5;
-  const occupancyShare = visitorCountEstimate / seatCount;
+  const occupancyShare = visitorCountEstimate / occupancyLimit || seatCount;
   if (occupancyShare <= 0.5) return "quiet";
   if (occupancyShare <= 0.7) return "busy";
   return "crowded";
