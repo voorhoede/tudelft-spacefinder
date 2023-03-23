af83597a15977c9a256e5c43552b84803381f99d
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-23 17:38:31 +0100

    style: braces for if

--- config/routes.ts
+++ config/routes.ts
@@ -21,17 +21,17 @@ for (const building of buildings) {
   routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/`);
 }
 
-if (process.env.SPACES_MODE == "rooms")
+if (process.env.SPACES_MODE == "rooms") {
   for (const room of rooms) {
     const building = buildingMap[room.buildingNumber];
     routes.push(`/en/buildings/${building.i18n.en.slug}/spaces/${room.slug}/`);
     routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/ruimtes/${room.slug}/`);
   }
-else
+} else {
   for (const space of spaces) {
     const building = buildingMap[space.buildingNumber];
     routes.push(`/en/buildings/${building.i18n.en.slug}/spaces/${space.slug}/`);
     routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/ruimtes/${space.slug}/`);
   }
-
+}
 export default routes;
