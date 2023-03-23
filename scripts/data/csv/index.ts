f476ca28dc7ab8315ee177ae568d12dda177d2ee
Mikhail Kirillov <mikhail.kirillov@outlook.com>
2023-03-23 15:36:38 +0100

    fix: treat data inconcistency as warnings

--- scripts/data/csv/index.ts
+++ scripts/data/csv/index.ts
@@ -18,8 +18,9 @@ export function buildingNumberFromId(buildingId: string) {
 }
 
 export function getData(csvPath: string) {
-  return fs.readFile(csvPath)
-    .then((data) => parse(data, parserOptions))
+  return fs
+    .readFile(csvPath)
+    .then((data) => parse(data, parserOptions) as Record<string, any>[]);
 }
 
 export function transform(
@@ -57,38 +58,58 @@ export function transform(
       realEstateNumbersForBuilding.push(source.realEstateNumber);
       building.totalRooms++;
     }
-
-    if (!(source.realEstateNumber in rooms))
+    if (parseInt(source.realEstateNumber.substring(0, 2)) != buildingNumber)
+      console.warn(
+        `Property ID invalid: ${source.realEstateNumber} doesn't start with building number ${buildingNumber}`
+      );
+    if (!(source.realEstateNumber in rooms)) {
+      const room = Object.values(rooms).find(
+        (room) =>
+          room.roomId == source.roomId && room.buildingNumber == buildingNumber
+      );
+      if (room)
+        console.warn(
+          `Room ID ${source.roomId} in building ${buildingNumber} appears for both ${source.realEstateNumber} and ${room.realEstateNumber}`
+        );
       rooms[source.realEstateNumber] = getRoom(buildingNumber, source);
-    else {
+    } else {
       //TODO: remove these checks later
       const room = rooms[source.realEstateNumber];
       if (room.floor != source.floor)
-        throw `Different floors for room ${source.realEstateNumber}`;
+        console.warn(
+          `Found zones with different floors for room ${source.realEstateNumber}: ${room.floor}, ${source.floor}`
+        );
       if (room.roomId != source.roomId)
-        throw `Different room IDs for room ${source.realEstateNumber}: ${room.roomId}, ${source.roomId}`;
+        console.warn(
+          `Found zones with different room IDS for room ${source.realEstateNumber}: ${room.roomId}, ${source.roomId}`
+        );
 
       if (room.facilities.studyType != source.studyType)
-        throw `Different study types for room ${source.realEstateNumber}: ${room.facilities.studyType}, ${source.studyType}`;
+        console.warn(
+          `Found zones with different study types for room ${source.realEstateNumber}: ${room.facilities.studyType}, ${source.studyType}`
+        );
 
       if (room.facilities.quietness != source.quietness)
-        throw `Different quietness for room ${source.realEstateNumber}: ${room.facilities.quietness}, ${source.quietness}`;
+        console.warn(
+          `Found zones with different quietness for room ${source.realEstateNumber}: ${room.facilities.quietness}, ${source.quietness}`
+        );
 
       if (room.exchangeBuildingId != source.exchangeBuildingId)
-        throw `Different exchangeBuildingId for room ${source.realEstateNumber}: ${room.exchangeBuildingId}, ${source.exchangeBuildingId}`;
+        console.warn(
+          `Found zones with different exchangeBuildingId for room ${source.realEstateNumber}: ${room.exchangeBuildingId}, ${source.exchangeBuildingId}`
+        );
 
       if (room.exchangeRoomId != source.exchangeRoomId)
-        throw `Different exchangeRoomId for room ${source.realEstateNumber}: ${room.exchangeRoomId}, ${source.exchangeRoomId}`;
+        console.warn(
+          `Found zones with different exchangeRoomId for room ${source.realEstateNumber}: ${room.exchangeRoomId}, ${source.exchangeRoomId}`
+        );
 
       if (room.i18n.nl.name != source.spaceNameNL.trim())
-        throw `Different NL names for room ${source.realEstateNumber}: ${
-          room.i18n.nl.name
-        }, ${source.spaceNameNL.trim()}`;
-
-      if (room.i18n.en.name != source.spaceNameEN.trim())
-        throw `Different EN names for room ${source.realEstateNumber}: ${
-          room.i18n.en.name
-        }, ${source.spaceNameEN.trim()}`;
+        console.warn(
+          `Found zones with different NL names for room ${
+            source.realEstateNumber
+          }: ${room.i18n.nl.name}, ${source.spaceNameNL.trim()}`
+        );
     }
     const room = rooms[source.realEstateNumber];
     room.seats += source.seats;
