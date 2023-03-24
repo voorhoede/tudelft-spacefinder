7b99af108567377e461d07f938347e6c68918287
Selwyn <talk@selwyn.cc>
2023-03-24 13:48:43 +0100

    Get routes during build so Nuxt prepare does not rely on data

--- config/routes.ts
+++ config/routes.ts
@@ -1,37 +1,40 @@
-import buildings from "../src/data/buildings.json";
-import rooms from "../src/data/rooms.json";
-import spaces from "../src/data/spaces.json";
-const buildingMap = {} as Record<number, any>;
-for (const building of buildings) buildingMap[building.number] = building;
+export function generateRoutes(
+  { buildings, spaces, rooms }:
+  { buildings: unknown, spaces: unknown, rooms: unknown }
+) {
+  const buildingMap = {} as Record<number, any>;
 
-//TODO: rewrite after getting rid of vue-i18n
-const routes = [
-  "/en/",
-  "/en/buildings/",
-  "/en/help/",
-  "/en/feedback/",
-  "/nl/",
-  "/nl/gebouwen/",
-  "/nl/hulp/",
-  "/nl/feedback/",
-];
+  for (const building of buildings) buildingMap[building.number] = building;
 
-for (const building of buildings) {
-  routes.push(`/en/buildings/${building.i18n.en.slug}/`);
-  routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/`);
-}
+  const routes = [
+    "/en/",
+    "/en/buildings/",
+    "/en/help/",
+    "/en/feedback/",
+    "/nl/",
+    "/nl/gebouwen/",
+    "/nl/hulp/",
+    "/nl/feedback/",
+  ];
 
-if (process.env.SPACES_MODE == "rooms") {
-  for (const room of rooms) {
-    const building = buildingMap[room.buildingNumber];
-    routes.push(`/en/buildings/${building.i18n.en.slug}/spaces/${room.slug}/`);
-    routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/ruimtes/${room.slug}/`);
+  for (const building of buildings) {
+    routes.push(`/en/buildings/${building.i18n.en.slug}/`);
+    routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/`);
   }
-} else {
-  for (const space of spaces) {
-    const building = buildingMap[space.buildingNumber];
-    routes.push(`/en/buildings/${building.i18n.en.slug}/spaces/${space.slug}/`);
-    routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/ruimtes/${space.slug}/`);
+
+  if (process.env.SPACES_MODE == "rooms") {
+    for (const room of rooms) {
+      const building = buildingMap[room.buildingNumber];
+      routes.push(`/en/buildings/${building.i18n.en.slug}/spaces/${room.slug}/`);
+      routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/ruimtes/${room.slug}/`);
+    }
+  } else {
+    for (const space of spaces) {
+      const building = buildingMap[space.buildingNumber];
+      routes.push(`/en/buildings/${building.i18n.en.slug}/spaces/${space.slug}/`);
+      routes.push(`/nl/gebouwen/${building.i18n.nl.slug}/ruimtes/${space.slug}/`);
+    }
   }
+
+  return routes;
 }
-export default routes;
--- src/server/routes/sitemap.xml.ts
+++ src/server/routes/sitemap.xml.ts
@@ -1,5 +1,11 @@
 import { SitemapStream, streamToPromise } from "sitemap";
-import routes from "../../../config/routes";
+import { generateRoutes } from "../../../config/routes";
+import buildings from "../../data/buildings.json";
+import spaces from "../../data/spaces.json";
+import rooms from "../../data/rooms.json";
+
+const routes = generateRoutes({ buildings, spaces, rooms });
+
 export default defineEventHandler(async (event) => {
   const runtimeConfig = useRuntimeConfig();
   const sitemap = new SitemapStream({
