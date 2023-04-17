07e3cfbf331602c460ebc4bdef96ccece437ff94
Selwyn <talk@selwyn.cc>
2023-04-17 16:47:44 +0200

    Attempt to parse building number so fallback is used for invalid values

--- src/server/helpers/parse-message.ts
+++ src/server/helpers/parse-message.ts
@@ -6,8 +6,8 @@ export function parseMessage(
     "updated_at": new Date(Number(timestamp)).toISOString(),
     "access_point_name": decodedValue.name,
     "device_count": decodedValue.clientCount,
-    "building_number": decodedValue.name.split("-").at(1)
-      || decodedValue.mapLocation.split(".").at(0)
+    "building_number": Number(decodedValue.name.split("-").at(1))
+      || Number(decodedValue.mapLocation.split(".").at(0))
       || 0,
     "floor": decodedValue.locationHierarchy.split(">").at(-1).trim(),
     "room_id": decodedValue.mapLocation.split(" ").at(0),
