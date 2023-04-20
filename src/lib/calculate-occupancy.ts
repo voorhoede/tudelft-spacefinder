fa915df906b745769106bfa1899309f659e18d83
Zowie van Geest <zowie@zowievangeest.nl>
2023-04-20 16:39:07 +0200

    refactor: calculate-occupancy to have unknown as occupancy

--- src/lib/calculate-occupancy.ts
+++ src/lib/calculate-occupancy.ts
@@ -1,20 +1,28 @@
 import type { Occupancy } from "../types/Filters";
 
+type Location = {
+  occupancyLimit?: number | null;
+  seats?: number;
+  totalSeats?: number;
+};
+
 export default function calculateOccupancy(
   deviceCount: number | null | undefined,
-  location: {
-    occupancyLimit?: number | null;
-    seats?: number;
-    totalSeats?: number;
-  }
+  location: Location
 ): Occupancy | undefined {
-  if (deviceCount == undefined) return undefined;
+  if (deviceCount === undefined || deviceCount === null) return undefined;
+
   const visitorCountEstimate = deviceCount / 2.5;
   const capacity =
-    location.occupancyLimit || location.seats || location.totalSeats;
+    location.occupancyLimit ?? location.seats ?? location.totalSeats;
+
   if (!capacity) return "crowded";
+
   const occupancyShare = visitorCountEstimate / capacity;
+
   if (occupancyShare <= 0.5) return "quiet";
   if (occupancyShare <= 0.7) return "busy";
-  return "crowded";
+  if (occupancyShare <= 1) return "crowded";
+
+  return "unknown";
 }
