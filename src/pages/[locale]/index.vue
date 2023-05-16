46cc06f5c8c411740b663cfc6425f6d592c9988e
Zowie van Geest <zowie@zowievangeest.nl>
2023-05-16 16:18:41 +0200

    feat: add building as default "route" (#219)

--- src/pages/[locale]/index.vue
+++ src/pages/[locale]/index.vue
@@ -1,30 +1,55 @@
 <template>
   <section
     v-if="!$isMobile.value"
-    class="default-layout__info"
+    class="default-layout__info building-overview"
   >
     <h2 class="a11y-sr-only">
-      {{ allSpacesTitle }}
+      {{ title }}
     </h2>
-    <SpaceList :spaces="spacesStore.filteredSpaces" />
+
+    <BuildingCard
+      v-for="building in buildingsOrdered"
+      :key="building.number"
+      :building="building"
+    />
   </section>
 </template>
 
 <script setup lang="ts">
 import { useSpacesStore } from "~/stores/spaces";
 import { useMapStore } from "~/stores/map";
-const { $t } = useNuxtApp();
 const spacesStore = useSpacesStore();
+
 const mapStore = useMapStore();
+const { $t } = useNuxtApp();
 
-const allSpacesTitle = computed(() => $t("allSpaces"));
+const buildingsOrdered = computed(() =>
+  [...spacesStore.buildings].sort((a, b) => a.name.localeCompare(b.name))
+);
 
-useSpacefinderHead({
-  title: computed(() => $t("spacesTitle")),
-  description: allSpacesTitle,
-});
+const title = computed(() => $t("buildingTitle"));
+
+useSpacefinderHead({ title });
 
 onMounted(() => {
   mapStore.restoreMapState();
 });
 </script>
+
+<style>
+.building-overview {
+  padding: var(--spacing-default);
+  height: calc(100% - var(--navigation-height-mobile));
+  -webkit-overflow-scrolling: touch;
+}
+
+@media (min-width: 700px) {
+  .building-overview {
+    height: calc(100% - var(--navigation-height-desktop));
+  }
+}
+
+.building-overview .building-card:not(:last-child) {
+  margin-bottom: var(--spacing-default);
+}
+</style>
