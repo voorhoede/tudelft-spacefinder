31737f613336f8db9a367a0edd04586ce524252f
Selwyn <talk@selwyn.cc>
2023-07-12 17:26:07 +0200

    Add script to help debug incoming Kafka data

---
+++ scripts/kafka-debugging.mjs
@@ -0,0 +1,59 @@
+import "dotenv/config";
+import { Kafka } from "kafkajs";
+import { SchemaRegistry } from "@kafkajs/confluent-schema-registry";
+
+const kafkaConfig = {
+  url: process.env.KAFKA_URL,
+  username: process.env.KAFKA_USERNAME,
+  password: process.env.KAFKA_PASSWORD,
+  groupId: "tud_wifi_jasper_moelker",
+  topics: [
+    "tud_aruba_access_point_client_counts",
+    "tud_wifi_access_point_details",
+  ],
+};
+const schemaRegistry = {
+  url: process.env.SCHEMA_REGISTRY_URL,
+  username: process.env.SCHEMA_REGISTRY_USERNAME,
+  password: process.env.SCHEMA_REGISTRY_PASSWORD,
+};
+
+console.time("Initialize");
+const kafka = new Kafka({
+  clientId: "tudelft-spacefinder",
+  ssl: true,
+  sasl: {
+    mechanism: "plain",
+    username: kafkaConfig.username,
+    password: kafkaConfig.password,
+  },
+  brokers: [kafkaConfig.url],
+});
+
+const registry = new SchemaRegistry({
+  host: schemaRegistry.url,
+  auth: {
+    username: schemaRegistry.username,
+    password: schemaRegistry.password,
+  },
+});
+
+const consumer = kafka.consumer({ groupId: kafkaConfig.groupId });
+console.timeEnd("Initialize");
+
+await consumer.connect();
+await consumer.subscribe({ topics: kafkaConfig.topics, fromBeginning: false });
+
+await consumer.run({
+  eachMessage: async ({ message }) => {
+    registry
+      .decode(message.value)
+      .then(console.info)
+  },
+});
+
+process.on("SIGINT", async () => {
+  console.time("Consumer disconnect");
+  await consumer.disconnect();
+  console.timeEnd("Consumer disconnect");
+});
