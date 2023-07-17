e79cce94543ccc686bf6bc866d8ea3c6375414a9
Selwyn <talk@selwyn.cc>
2023-07-17 15:17:15 +0200

    Add back tests for wifi data

--- src/server/helpers/parse-message.ts
+++ src/server/helpers/parse-message.ts
@@ -1,16 +1,16 @@
 export function parseMessageWifi(
   { timestamp, decodedValue }:
-  { timestamp: string; decodedValue: Record<string, string> }
+  { timestamp: string; decodedValue: Record<string, string | number> }
 ) {
   return {
     "updated_at": new Date(Number(timestamp)).toISOString(),
     "access_point_name": decodedValue.name,
     "device_count": decodedValue.clientCount,
-    "building_number": Number(decodedValue.name.split("-").at(1))
-      || Number(decodedValue.mapLocation.split(".").at(0))
-      || 0,
-    "floor": decodedValue.locationHierarchy.split(">").at(-1)?.trim() || null,
-    "room_id": decodedValue.mapLocation.split(" ").at(0),
+    "building_number": Number(String(decodedValue.name).split("-").at(1))
+      || Number(String(decodedValue.mapLocation).split(".").at(0))
+      || null,
+    "floor": String(decodedValue.locationHierarchy).split(">").at(-1)?.trim() || null,
+    "room_id": String(decodedValue.mapLocation).split(" ").at(0),
     "map_location": decodedValue.mapLocation,
     "location_hierarchy": decodedValue.locationHierarchy,
   };
@@ -18,13 +18,13 @@ export function parseMessageWifi(
 
 export function parseMessageAruba(
   { timestamp, decodedValue }:
-  { timestamp: string; decodedValue: Record<string, string> }
+  { timestamp: string; decodedValue: Record<string, string | number> }
 ) {
   return {
     "updated_at": new Date(Number(timestamp)).toISOString(),
     "access_point_name": decodedValue.name,
     "device_count": decodedValue.client_count,
-    "building_number": Number(decodedValue.name.split("-").at(1)) || 0,
+    "building_number": Number(String(decodedValue.name).split("-").at(1)) || null,
   };
 }
 
