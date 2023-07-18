55b03ef7bbd5735112e102ec4d29aeafaad5486f
Bas de Vleeschhouwer <bdevleeschhouwer@protonmail.com>
2023-07-18 09:30:34 +0200

    Style notification panel in the same way as the notification bar, make sure it’s always on top

--- src/components/NotificationPanel/NotificationPanel.vue
+++ src/components/NotificationPanel/NotificationPanel.vue
@@ -32,22 +32,32 @@ function closePanel() {
 @import "../app-core/variables.css";
 
 .notification-panel {
+  z-index: var(--layer--popup);
+  position: absolute;
   display: flex;
   justify-content: space-between;
-  align-items: center;
-  padding: var(--spacing-three-quarter) var(--spacing-half) var(--spacing-three-quarter) var(--spacing-default);
+  align-items: flex-start;
+  padding: var(--spacing-default) var(--spacing-three-quarter) var(--spacing-default) var(--spacing-default);
+  bottom: var(--spacing-default);
+  left: var(--spacing-default);
+  width: calc(100% - var(--spacing-double));
   background: var(--brand-secondary-color);
+  border: 1px solid transparent;
+  box-shadow: var(--shadow-small);
   color: var(--background-color);
-  line-height: 1.2;
+  line-height: 1.3;
 }
 
 .notification-panel__content {
   max-width: 800px;
+  align-self: center;
 }
 
 @media (min-width: 700px) {
   .notification-panel {
-    padding: var(--spacing-three-quarter) var(--spacing-default);
+    bottom: var(--spacing-double);
+    left: var(--spacing-double);
+    width: calc(100% - 2 * var(--spacing-double));
   }
 }
 </style>
