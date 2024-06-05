8ce4aefe032f23da61a7c1b5a3a21fea8d012c12
Selwyn <talk@selwyn.cc>
2024-06-05 12:43:14 +0200

    Set building number on views to strings

--- src/server/helpers/parse-message.ts
+++ src/server/helpers/parse-message.ts
@@ -10,7 +10,13 @@ export function parseMessageAruba(
     "updated_at": new Date(Number(timestamp)).toISOString(),
     "access_point_name": decodedValue.name,
     "device_count": decodedValue.client_count,
-    "building_number": Number(String(decodedValue.name).split("-").at(1)) || null,
+    "building_number": parseBuildingNumber({ name: decodedValue.name as string }),
   };
 }
 
+function parseBuildingNumber({ name }: { name: string }) {
+  const rawBuildingNumber = Number(name.split("-").at(1));
+  return rawBuildingNumber
+    ? String(rawBuildingNumber).padStart(2, "0")
+    : null;
+}
