9dcb9f5e388e5191e5e9058327b29492c797815d
Selwyn <talk@selwyn.cc>
2024-06-05 17:49:56 +0200

    Update zoom-levels.ts

--- src/lib/zoom-levels.ts
+++ src/lib/zoom-levels.ts
@@ -1,6 +1,6 @@
 export default {
   zoom: 12, // default mapbox zoom level when initializing the map
-  minZoom: 15, // minimum zoom level when zooming out this is the 'lock'
+  minZoom: 14, // minimum zoom level when zooming out this is the 'lock'
   defaultZoom: 16.8, // default zoom level when no selection is made in zoomToBuilding and zoomToSpace 
   unClusterZoom: 16.8, // zoom level when unclustering so clusters become individual points / spaces
   buildingZoom: 16.8, // zoom level when zooming to a building in zoomToBuilding
