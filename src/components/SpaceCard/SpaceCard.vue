e4646668cd6956f210a5ebbbeb1e26df8dffa704
Selwyn <talk@selwyn.cc>
2025-07-11 16:32:14 +0200

    Make space image expandable to view details

--- src/components/SpaceCard/SpaceCard.vue
+++ src/components/SpaceCard/SpaceCard.vue
@@ -26,16 +26,28 @@
     </div>
 
     <div class="space-card__image-wrapper">
-      <DatoImage
+      <div
         v-if="space.image"
-        class="space-card__image"
-        :src="space.image.url"
-        alt=""
-        :width="210"
-        :height="158"
-        loading="eager"
-        :modifiers="{ fit: 'crop', ar: 1.33 }"
-      />
+        class="space-card__image-inner"
+      >
+        <DatoImage
+          class="space-card__image"
+          :class="{ 'space-card__image--zoomable': zoomable }"
+          :src="space.image.url"
+          alt=""
+          :width="210"
+          :height="158"
+          loading="eager"
+          :modifiers="{ fit: 'crop', ar: 1.33 }"
+          @click="zoomable && openDialog()"
+        />
+        <SvgIcon
+          class="space-card__image-icon"
+          v-if="zoomable"
+          name="arrows-pointing-out"
+          @click="openDialog()"
+        />
+      </div>
       <div
         v-else
         class="space-card__image space-card__image--placeholder"
@@ -46,13 +58,40 @@
         />
       </div>
     </div>
+
+    <dialog
+      v-if="zoomable"
+      ref="imageDialog"
+      class="space-card__dialog"
+      @click.self="closeDialog()"
+    >
+      <DatoImage
+        class="space-card__dialog-image"
+        :src="space.image.url"
+        sizes="min(100vw, 160dvh)"
+        alt=""
+        loading="lazy"
+        :width="1200"
+        :height="900"
+        :modifiers="{ fit: 'scale' }"
+      />
+      <button
+        class="space-card__dialog-close"
+        @click="closeDialog()"
+        aria-label="Close image"
+      >
+        <SvgIcon name="x-mark" />
+        {{ $t("close") }}
+      </button>
+    </dialog>
   </div>
 </template>
 
 <script setup lang="ts">
-import { SpaceFeatures } from "~/types/Filters";
+import { ref } from "vue";
+import { type SpaceFeatures } from "~/types/Filters";
 
-defineProps<{
+const props = defineProps<{
   space: {
     name: string;
     roomName: string;
@@ -66,7 +105,19 @@ defineProps<{
       abbreviation: string;
     };
   };
+  canZoom: boolean;
 }>();
+
+const imageDialog = ref<HTMLDialogElement>();
+const zoomable = computed(() => props.canZoom && props.space.image);
+
+const openDialog = () => {
+  imageDialog.value?.showModal();
+};
+
+const closeDialog = () => {
+  imageDialog.value?.close();
+};
 </script>
 
 <style>
@@ -119,6 +170,29 @@ defineProps<{
   height: 100%;
 }
 
+.space-card__image-inner {
+  display: grid;
+  grid-template-areas: "stack";
+  height: 100%;
+
+  * {
+    grid-area: stack;
+  }
+}
+
+.space-card__image-icon {
+  width: 24px;
+  height: 24px;
+  padding: var(--spacing-quarter);
+  box-sizing: content-box;
+  place-self: flex-end;
+  margin: var(--spacing-half);
+  cursor: pointer;
+  opacity: 0.9;
+  background: white;
+  border-radius: 4px;
+}
+
 .space-card__image--placeholder {
   display: flex;
   justify-content: center;
@@ -129,5 +203,72 @@ defineProps<{
 .space-card__image-placeholder-icon {
   width: 30px;
   height: 30px;
+}
+
+.space-card__image--zoomable {
+  cursor: pointer;
+}
+
+.space-card__dialog[open] {
+  display: flex;
+  flex-direction: column;
+  padding: 0;
+  border: 0;
+  background: none;
+  width: 100%;
+  max-width: 100%;
+  max-height: 100dvh;
+  height: 100%;
+  justify-content: center;
+  background: rgba(255, 255, 255, 0.8);
+  background: none;
+}
+
+.space-card__dialog::backdrop {
+  background: rgba(0, 0, 0, 0.4);
+  backdrop-filter: blur(8px);
+}
+
+.space-card__dialog-close {
+  display: flex;
+  align-self: flex-end;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  max-width: 12rem;
+  padding-inline: var(--spacing-half);
+  padding-block: var(--spacing-three-quarter);
+  cursor: pointer;
+  background: var(--background-color);
+  box-shadow: var(--shadow-small);
+  border: none;
+  border-radius: 2rem;
+  position: absolute;
+  bottom: var(--spacing-default);
+  right: var(--spacing-default);
+}
+
+.space-card__dialog-close:hover {
+  background: rgba(255, 255, 255, 1);
+}
+
+.space-card__dialog-close svg {
+  width: 24px;
+  height: 24px;
+}
+
+.space-card__dialog-image {
+  object-fit: contain;
+  align-self: center;
+  max-height: 90dvh;
+  width: 100%;
+  flex-basis: 100%;
+}
+
+@media (min-width: 700px) {
+  .space-card__dialog-close {
+    top: var(--spacing-default);
+    bottom: auto;
+  }
 }
 </style>
